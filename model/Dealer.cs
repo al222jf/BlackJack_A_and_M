0666afc8e6e0f4393a1fe2e36e16bd27d71ce2ab
mj223dg <mj223dg@student.lnu.se>
2015-10-28 16:41:01 +0100

    Fixed some issues in "Variations of who wins the game"

--- model/Dealer.cs
+++ model/Dealer.cs
@@ -12,12 +12,14 @@ namespace BlackJack.model
 
         private rules.INewGameStrategy m_newGameRule;
         private rules.IHitStrategy m_hitRule;
+        private rules.IEqualScoreStrategy m_winnerRule;
 
 
         public Dealer(rules.RulesFactory a_rulesFactory)
         {
             m_newGameRule = a_rulesFactory.GetNewGameRule();
             m_hitRule = a_rulesFactory.GetHitRule();
+            m_winnerRule = a_rulesFactory.EqualScoreRule();
         }
 
         public bool NewGame(Player a_player)
@@ -66,15 +68,7 @@ namespace BlackJack.model
 
         public bool IsDealerWinner(Player a_player)
         {
-            if (a_player.CalcScore() > g_maxScore)
-            {
-                return true;
-            }
-            else if (CalcScore() > g_maxScore)
-            {
-                return false;
-            }
-            return CalcScore() >= a_player.CalcScore();
+            return m_winnerRule.winner(a_player, this, g_maxScore);
         }
 
         public bool IsGameOver()
